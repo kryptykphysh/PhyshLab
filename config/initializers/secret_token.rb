397412abda15c9596b3163b49f2f8a8940ff1438
Kryptyk Fysh <github@kryptykfysh.co.uk>
2012-08-21 11:13:30 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhyshLab::Application.config.secret_token = '8e6ab9bc52e336e383b664998cffc209cbb496d312b0027ccb9c52de3c5f0172031d2d2de7eba5b2262651256069685ad12dc8999922e66be9f4d93d51f0b26d'
